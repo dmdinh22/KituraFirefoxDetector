c805f2f29de325d8836c3fdd4d62f1dd065dd949
David Dinh <dmdinh22@gmail.com>
2019-03-17 21:24:56 -0700

    create ff detector middleware, resolved dependencies for kitura

--- Sources/KituraFirefoxDetector/KituraFirefoxDetector.swift
+++ Sources/KituraFirefoxDetector/KituraFirefoxDetector.swift
@@ -1,3 +1,20 @@
-struct KituraFirefoxDetector {
-    var text = "Hello, World!"
+
+import Kitura
+import Foundation
+
+public class FirefoxDetector: RouterMiddleware {
+    // swift implicitly defines access level of internal,
+    // so need to expose this method to be used in other prodj
+    public func handle(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws {
+        if let userAgent = request.headers["User-Agent"], userAgent.contains("Firefox") {
+            request.userInfo["usingFirefox"] = true
+        }
+        else {
+            request.userInfo["usingFirefox"] = false
+        }
+        next()
+    }
+
+    public init () {
+    }
 }
